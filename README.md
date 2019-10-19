a236aa08edc809d141a8f6cd576f5007bfeadf05
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-19 18:08:28 +0300

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# React
-This project was made master create-react-app,
-class based functions
-
-## Used 
-create-react-app
-https://github.com/facebook/create-react-app
---
+++ precache-manifest.c86de0dfd6f3d89ca739eb260f9fea90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d94e9af3526834d7c3b341ef85960e0",
+    "url": "/react-apps-speedgame/index.html"
+  },
+  {
+    "revision": "fa4af6f55f2c1f11ff2c",
+    "url": "/react-apps-speedgame/static/css/main.8d3c131e.chunk.css"
+  },
+  {
+    "revision": "fbe9655b21b372d8a85c",
+    "url": "/react-apps-speedgame/static/js/2.97aeffc1.chunk.js"
+  },
+  {
+    "revision": "fa4af6f55f2c1f11ff2c",
+    "url": "/react-apps-speedgame/static/js/main.7e26b42b.chunk.js"
+  },
+  {
+    "revision": "135617d7f68c8d95b4c9",
+    "url": "/react-apps-speedgame/static/js/runtime~main.7600015a.js"
+  },
+  {
+    "revision": "fd3f395fb4edc0b3a8eee9ba92c39d40",
+    "url": "/react-apps-speedgame/static/media/mouse.fd3f395f.png"
+  }
+]);
